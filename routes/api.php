8c868c66530ab2688ad86f3ea1f7cef79253c094
fish <22388145+chaoyenpo@users.noreply.github.com>
2021-04-20 13:56:32 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::any('/webhook', [ForwardController::class, 'receive']);
+Route::post('/webhook', [ForwardController::class, 'receive']);
