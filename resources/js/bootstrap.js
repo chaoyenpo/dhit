0224c9df4c0fa1ca364bfe18ab90388d27dc0d43
fish <22388145+chaoyenpo@users.noreply.github.com>
2021-04-29 16:19:59 +0800

    Update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,7 +24,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    wsHost: window.location.hostname,
+    wsHost: process.env.MIX_PUSHER_HOST,
     wsPort: 6001,
     disableStats: true,
     forceTLS: false,
