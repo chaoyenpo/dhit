dfca70d81f1eaa041a6521b4c1a2ce15d85723bf
Fish <fish@glegame.com>
2021-06-10 17:55:02 +0800

    Update

---
+++ resources/js/Pages/Domain/DomainTable.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="flex mb-5">
+    <jet-button type="button">
+      刪除
+    </jet-button>
+  </div>
+
+  <div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  <jet-checkbox name="all" />
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  網域名稱
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  標籤
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  域名到期時間
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  憑證到期時間
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(domain, domainIdx) in $page.props.domains"
+                :key="domain.name"
+                :class="domainIdx % 2 === 0 ? 'bg-white' : 'bg-gray-50'"
+              >
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  <jet-checkbox name="all" />
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {{ domain.name }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ domain.tag }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ domain.domain_expired_at }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ domain.certificate_expired_at }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import JetCheckbox from "@/Jetstream/Checkbox";
+import JetButton from "@/Jetstream/Button";
+
+export default {
+  components: {
+    JetCheckbox,
+    JetButton,
+  },
+
+  data() {
+    return {
+      //
+    };
+  },
+
+  methods: {
+    //
+  },
+};
+</script>
--- resources/js/Pages/Domain/Show.vue
+++ resources/js/Pages/Domain/Show.vue
@@ -36,64 +36,7 @@
         <div v-if="$page.props.domains?.length > 0">
           <jet-section-border />
 
-          <div class="flex flex-col">
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                  <table class="min-w-full divide-y divide-gray-200">
-                    <thead class="bg-gray-50">
-                      <tr>
-                        <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          網域名稱
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          標籤
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          域名到期時間
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                        >
-                          憑證到期時間
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr
-                        v-for="(domain, domainIdx) in $page.props.domains"
-                        :key="domain.name"
-                        :class="domainIdx % 2 === 0 ? 'bg-white' : 'bg-gray-50'"
-                      >
-                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {{ domain.name }}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {{ domain.tag }}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {{ domain.domain_expired_at }}
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {{ domain.certificate_expired_at }}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
+          <domain-table />
         </div>
       </div>
     </div>
@@ -101,13 +44,14 @@
 </template>
 
 <script>
-import { useForm } from "@inertiajs/inertia-vue3";
 import AppLayout from "@/Layouts/AppLayout";
 import JetButton from "@/Jetstream/Button";
 import JetResponsiveNavLink from "@/Jetstream/ResponsiveNavLink";
 import UploadDomainForm from "./UploadDomainForm";
 import JetActionSection from "@/Jetstream/ActionSection";
 import JetSectionBorder from "@/Jetstream/SectionBorder";
+import JetCheckbox from "@/Jetstream/Checkbox";
+import DomainTable from "./DomainTable";
 
 export default {
   components: {
@@ -117,6 +61,8 @@ export default {
     UploadDomainForm,
     JetActionSection,
     JetSectionBorder,
+    JetCheckbox,
+    DomainTable,
   },
 
   data() {
